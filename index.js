b01ba0f1d2581306a1ac0e7d334777972891db9d
Phil Brookes <phil.brookes@redhat.com>
2015-05-15 10:37:49 +0100

    adding UI

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var swig = require('swig');
+var bodyParser = require('body-parser')
 
+//mongoose.connect('mongodb://172.30.17.249');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res){
   res.send(
@@ -12,8 +19,32 @@ app.get("/", function(req, res){
   );
 });
 
-//app.use(express.static('public'));
+app.get("/user", function(req, res){
+  res.send(
+    swig.renderFile('./templates/user.swig', {
+      user: {}
+    })
+  );
+});
+
+app.get("/user/:id", function(req, res){
+  res.send(
+    swig.renderFile('./templates/user.swig', {
+      user: {
+        id: req.params.id,
+        name: "phil"
+      }
+    })
+  );
+});
+
+app.post("/user", function(req, res){
+  res.send(req.body);
+});
+
+app.post("/user/:id", function(req, res){
+  res.send(req.body.name);
+});
 
 http.listen(8080, function(){ console.log("ready!")});
 
-//mongoose.connect('mongodb://172.30.17.249');
