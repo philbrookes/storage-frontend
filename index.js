2eefb8ae47d3929c1e1dd85dd6991ba29098fe54
Phil Brookes <phil.brookes@redhat.com>
2015-05-15 09:27:18 +0100

    comimt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 app.use(express.static('public'));
 
+http.listen(8080, function(){ console.log("ready!")});
